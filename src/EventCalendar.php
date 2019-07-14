87489b309548e113f8c3c67a53f1f3668598e555
Klaus Krippeit <snyder3@web.de>
2019-07-14 23:37:06 +0200

    wip

--- src/EventCalendar.php
+++ src/EventCalendar.php
@@ -77,7 +77,7 @@ class EventCalendar {
             $eventOne = new CalendarEvent();
             $eventOne->setStart($date)
                 ->setSummary($summary)
-                ->setDescription($event['location'] . ' | ' . $event['text'])
+                ->setDescription($event['location'] . ' | ' . htmlspecialchars_decode($event['text']))
                 ->setUid($eventId);
             $calendar->addEvent($eventOne);
 
